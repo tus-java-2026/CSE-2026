26d884d3087e079f85684a568a835227a9af0f4b
A0025443HinaKausar <A0025443@student.tus.ie>
2026-07-17 00:16:43 +0100

    Example for loop

---
+++ Week3/src/examPractice/test123.java
@@ -0,0 +1,5 @@
+package examPractice;
+
+public class test123 {
+
+}
---
+++ Week3/src/w4_LoopsLogic/LoopForLogic.java
@@ -0,0 +1,10 @@
+package w4_LoopsLogic;
+
+public class LoopForLogic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

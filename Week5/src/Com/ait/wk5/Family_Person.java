8696268ac3910d8e3fd9a8cb12624c1d0cf347ef
A0025443HinaKausar <A0025443@student.tus.ie>
2026-07-27 18:58:15 +0100

    todays's work added week6

---
+++ Week5/src/Com/ait/wk5/Family_Person.java
@@ -0,0 +1,65 @@
+package Com.ait.wk5;
+
+public class Family_Person {
+
+	
+	// Instance variables (attributes)
+    private int age ;
+    private String name;
+    private String address;
+    private int count;
+    
+    
+ // Default constructor
+    public Family_Person() {
+        age = "00";
+        name = " ";
+        address = " ";
+        count = "00"
+    
+      
+    
+    // constructor with parameters
+        		
+    public Family_Person(int age, String name, String address, int count) {
+		super();
+		this.age = age;
+		this.name = name;
+		this.address = address;
+		this.count = count;
+	}
+
+	// Initialize variables
+    
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public Family_Person(int age, String name, String address, int count) {
+		super();
+		this.age = age;
+		this.name = name;
+		this.address = address;
+		this.count = count;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

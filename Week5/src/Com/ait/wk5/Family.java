9a5767dea58dc742898fc39ebbd6b411a0434fce
A0025443HinaKausar <A0025443@student.tus.ie>
2026-07-27 19:04:23 +0100

    updated work

--- Week5/src/Com/ait/wk5/Family.java
+++ Week5/src/Com/ait/wk5/Family.java
@@ -2,9 +2,40 @@ package Com.ait.wk5;
 
 public class Family {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	    public static void main(String[] args) {
 
+	        Family_Person john = new Family_Person(22, "John", "Dublin", 0);
+	        Family_Person ann = new  Family_Person(25, "Ann", "Galway", 0);
+
+	        // John's details
+	        System.out.println("John's Details");
+	        System.out.println("Name: " + john.getName());
+	        System.out.println("Age: " + john.getAge());
+	        System.out.println("Address: " + john.getAddress());
+
+	        System.out.println();
+
+	        // Ann's details
+	        System.out.println("Ann's Details");
+	        System.out.println("Name: " + ann.getName());
+	        System.out.println("Age: " + ann.getAge());
+	        System.out.println("Address: " + ann.getAddress());
+
+	        System.out.println();
+
+	        // Number of people created
+	        System.out.println("People created: " +  Family_Person.getCount());
+
+	        System.out.println();
+
+	        // Change John's name to jack
+	        john.setName("Jack");
+	        john.setAge(23);
+	        john.setAddress("Cork");
+
+	        System.out.println("Updated John's Details");
+	        System.out.println("Name: " + john.getName());
+	        System.out.println("Age: " + john.getAge());
+	        System.out.println("Address: " + john.getAddress());
+	    }
 	}
-
-}
--- Week5/src/Com/ait/wk5/Family_Person.java
+++ Week5/src/Com/ait/wk5/Family_Person.java
@@ -7,59 +7,54 @@ public class Family_Person {
     private int age ;
     private String name;
     private String address;
-    private int count;
+    private static int count;
     
     
  // Default constructor
     public Family_Person() {
-        age = "00";
+        age = 00;
         name = " ";
         address = " ";
-        count = "00"
-    
-      
+        count = 00;
+        
+    } 
     
     // constructor with parameters
         		
     public Family_Person(int age, String name, String address, int count) {
-		super();
 		this.age = age;
 		this.name = name;
 		this.address = address;
-		this.count = count;
+		Family_Person.count = count;
 	}
 
-	// Initialize variables
-    
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	public Family_Person(int age, String name, String address, int count) {
-		super();
+	// getters and setters
+   
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
 		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
 		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
 		this.address = address;
-		this.count = count;
 	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static int getCount() {
+		return count;
 	}
-
+	public void setCount(int count) {
+		Family_Person.count = count;
+	}
 }
+	
+
+	
